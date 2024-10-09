d470e3fcc3770250b0a6c411e32646685496df15
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-10-09 00:08:30 -0300

    Generar PDF

--- src/app/solicitud-respuesta/solicitud-respuesta.component.ts
+++ src/app/solicitud-respuesta/solicitud-respuesta.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SolicitudInspeccionService } from '../servicios/solicitud-inspeccion.service';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-solicitud-respuesta',
@@ -20,6 +22,22 @@ export class SolicitudRespuestaComponent implements OnInit{
   ngOnInit() {
     console.log(this.respuesta);
     this.respuesta = this.solicitudInspService.getResponse();
-    
-}
+  }
+
+  generatePDF() {
+    const data: HTMLElement | null = document.getElementById('contentToConvert');
+    html2canvas(data!).then(canvas => {
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const heightLeft = imgHeight;
+  
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+  
+      let position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('dynamicData.pdf'); // Generated PDF
+    });
+  }
 }
